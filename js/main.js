da4b1539b8c2fca8664a3b2585c1306f39276b1e
Hernan Oliva <hhernanoliva@gmail.com>
2020-07-27 16:20:59 -0300

    Tab Bar prueba

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // ** Loader **
-window.addEventListener("load", function(){
-    document.getElementById("loader").classList.toggle("loader2");
+window.addEventListener("load", function () {
+  document.getElementById("loader").classList.toggle("loader2");
 });
 
 
@@ -8,67 +8,198 @@ window.addEventListener("load", function(){
 // ** Particula Boton Social Media **
 
 if (document.body.animate) {
-    document.querySelector('#clicked').addEventListener('click', pop);
-  }
-  
-  function pop (e) {
-    // Quick check if user clicked the button using a keyboard
-    if (e.clientX === 0 && e.clientY === 0) {
-      const bbox = document.querySelector('#button').getBoundingClientRect();
-      const x = bbox.left + bbox.width / 2;
-      const y = bbox.top + bbox.height / 2;
-      for (let i = 0; i < 30; i++) {
-        // We call the function createParticle 30 times
-        // We pass the coordinates of the button for x & y values
-        createParticle(x, y);
-      }
-    } else {
-      for (let i = 0; i < 30; i++) {
-        // We call the function createParticle 30 times
-        // As we need the coordinates of the mouse, we pass them as arguments
-        createParticle(e.clientX, e.clientY);
-      }
+  document.querySelector('#clicked').addEventListener('click', pop);
+}
+
+function pop(e) {
+  // Quick check if user clicked the button using a keyboard
+  if (e.clientX === 0 && e.clientY === 0) {
+    const bbox = document.querySelector('#button').getBoundingClientRect();
+    const x = bbox.left + bbox.width / 2;
+    const y = bbox.top + bbox.height / 2;
+    for (let i = 0; i < 30; i++) {
+      // We call the function createParticle 30 times
+      // We pass the coordinates of the button for x & y values
+      createParticle(x, y);
+    }
+  } else {
+    for (let i = 0; i < 30; i++) {
+      // We call the function createParticle 30 times
+      // As we need the coordinates of the mouse, we pass them as arguments
+      createParticle(e.clientX, e.clientY);
     }
   }
-  
-  function createParticle (x, y) {
-    const particle = document.createElement('particle');
-    document.body.appendChild(particle);
-    
-    // Calculate a random size from 5px to 25px
-    const size = Math.floor(Math.random() * 20 + 5);
-    particle.style.width = `${size}px`;
-    particle.style.height = `${size}px`;
-    // Generate a random color in a blue/purple palette
-    particle.style.background = `hsl(${Math.random() * 500 + 350}, 70%, 60%)`;
-    
-    // Generate a random x & y destination within a distance of 75px from the mouse
-    const destinationX = x + (Math.random() - 0.5) * 4 * 75;
-    const destinationY = y + (Math.random() - 0.5) * 4 * 75;
-  
-    // Store the animation in a variable as we will need it later
-    const animation = particle.animate([
-      {
-        // Set the origin position of the particle
-        // We offset the particle with half its size to center it around the mouse
-        transform: `translate(-50%, -50%) translate(${x}px, ${y}px)`,
-        opacity: 1
-      },
-      {
-        // We define the final coordinates as the second keyframe
-        transform: `translate(${destinationX}px, ${destinationY}px)`,
-        opacity: 0
-      }
-    ], {
-      // Set a random duration from 500 to 1500ms
-      duration: Math.random() * 1000 + 500,
-      easing: 'cubic-bezier(0, .9, .57, 1)',
-      // Delay every particle with a random value of 200ms
-      delay: Math.random() * 200
-    });
-    
-    // When the animation is complete, remove the element from the DOM
-    animation.onfinish = () => {
-      particle.remove();
-    };
+}
+
+function createParticle(x, y) {
+  const particle = document.createElement('particle');
+  document.body.appendChild(particle);
+
+  // Calculate a random size from 5px to 25px
+  const size = Math.floor(Math.random() * 20 + 5);
+  particle.style.width = `${size}px`;
+  particle.style.height = `${size}px`;
+  // Generate a random color in a blue/purple palette
+  particle.style.background = `hsl(${Math.random() * 500 + 350}, 70%, 60%)`;
+
+  // Generate a random x & y destination within a distance of 75px from the mouse
+  const destinationX = x + (Math.random() - 0.5) * 15 * 75;
+  const destinationY = y + (Math.random() - 0.5) * 15 * 75;
+
+  // Store the animation in a variable as we will need it later
+  const animation = particle.animate([
+    {
+      // Set the origin position of the particle
+      // We offset the particle with half its size to center it around the mouse
+      transform: `translate(-50%, -50%) translate(${x}px, ${y}px)`,
+      opacity: 1
+    },
+    {
+      // We define the final coordinates as the second keyframe
+      transform: `translate(${destinationX}px, ${destinationY}px)`,
+      opacity: 0
+    }
+  ], {
+    // Set a random duration from 500 to 1500ms
+    duration: Math.random() * 15000 + 500,
+    easing: 'cubic-bezier(0, .9, .57, 1)',
+    // Delay every particle with a random value of 200ms
+    delay: Math.random() * 100
+  });
+
+  // When the animation is complete, remove the element from the DOM
+  animation.onfinish = () => {
+    particle.remove();
+  };
+}
+
+// ** Modal **
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+};
+
+
+// Modal para Slider
+
+function toggleSlider() {
+  var blur = document.getElementById('blur');
+  blur.classList.toggle('activePopup')
+  var pop = document.getElementById('popslider');
+  pop.classList.toggle('active2Popup')
+};
+
+
+document.getElementById('close-btn').addEventListener('click', function () {
+  document.getElementById("proyectos").click();
+});
+
+
+/*function closeProyectos() {
+  document.getElementById('close-btn').addEventListener('click', close);
+  function close(){
+    document.getElementById("proyectos").click();
   }
+}*/
+
+
+
+// Glider.js
+window.addEventListener('load',function(){
+  document.querySelector('.glider').addEventListener('glider-slide-visible', function(event){
+      var glider = Glider(this);
+      console.log('Slide Visible %s', event.detail.slide)
+  });
+  document.querySelector('.glider').addEventListener('glider-slide-hidden', function(event){
+      console.log('Slide Hidden %s', event.detail.slide)
+  });
+  document.querySelector('.glider').addEventListener('glider-refresh', function(event){
+      console.log('Refresh')
+  });
+  document.querySelector('.glider').addEventListener('glider-loaded', function(event){
+      console.log('Loaded')
+  });
+
+  window._ = new Glider(document.querySelector('.glider'), {
+      slidesToShow: 1, //'auto',
+      slidesToScroll: 1,
+      itemWidth: 150,
+      draggable: true,
+      scrollLock: false,
+      dots: '#dots',
+      rewind: true,
+      arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+      },
+      responsive: [
+          {
+              breakpoint: 800,
+              settings: {
+                  slidesToScroll: 'auto',
+                  itemWidth: 300,
+                  slidesToShow: 'auto',
+                  exactWidth: true
+              }
+          },
+          {
+              breakpoint: 700,
+              settings: {
+                  slidesToScroll: 4,
+                  slidesToShow: 4,
+                  dots: false,
+                  arrows: false,
+              }
+          },
+          {
+              breakpoint: 600,
+              settings: {
+                  slidesToScroll: 3,
+                  slidesToShow: 3
+              }
+          },
+          {
+              breakpoint: 500,
+              settings: {
+                  slidesToScroll: 2,
+                  slidesToShow: 2,
+                  dots: false,
+                  arrows: false,
+                  scrollLock: true
+              }
+          }
+      ]
+  });
+});
